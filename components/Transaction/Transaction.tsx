cad1d9e3537fa07876412ed80d10e7974445964d
Andrej Skalicky <andrejskalick3@gmail.com>
2022-05-26 11:39:01 +0200

    Implemented TransactionHook

--- components/Transaction/Transaction.tsx
+++ components/Transaction/Transaction.tsx
@@ -1,11 +1,15 @@
+import { useTransactions } from "../../hooks/useTransactions"
 import TransactionBody from "./TransactionBody"
 import TransactionHeader from "./TransactionHeader"
 
 const Transaction = () => {
+
+    const { transaction_data, isLoading } = useTransactions()
+
     return (
         <div >
             <TransactionHeader />
-            <TransactionBody />
+            <TransactionBody transactions={transaction_data} loading={isLoading} />
         </div>
     )
 }
